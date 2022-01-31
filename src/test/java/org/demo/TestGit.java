8bb9e02f9375e5312c018efd4ebc3dbf0649b61d
venkatajalam <venkatgvj007@gmail.com>
2022-01-31 18:27:17 +0530

    username

---
+++ src/test/java/org/demo/TestGit.java
@@ -0,0 +1,16 @@
+package org.demo;
+
+public class TestGit {
+	
+	private void username() {
+System.out.println("username");
+	}
+
+public static void main(String[] args) {
+	
+	TestGit tg = new TestGit();
+	tg.username();
+	
+}
+
+}
